dde2a66211401f3a67fa8537ae6bf1e2a07fa81f
zongyi.lu <zongyi.lu@cidana.com>
2024-02-20 17:31:40 +0800

    Add files.

---
+++ poke.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const util = require('util');
+
+function log(file, obj) {
+    fs.writeFileSync(file + '.txt', util.inspect(obj, {depth: null, maxArrayLength: null}), function() {});
+    fs.writeFileSync(file + '.json', JSON.stringify(obj), function() {});
+}
+
+const {Dex} = require('../pokemon-showdown');
+
+const formats = Dex.formats.all();
+log('Formats', formats);
+
+function log_gen(gen) {
+
+    dex = Dex.mod(gen);
+    prefix = gen + '/';
+
+    if (!fs.existsSync(gen))
+        fs.mkdirSync(gen);
+
+    const abilities = dex.abilities.all();
+    log(prefix + 'List_Abilities', abilities);
+
+    const items = dex.items.all();
+    log(prefix + 'List_Items', items);
+
+    const moves = dex.moves.all();
+    log(prefix + 'List_Moves', moves);
+
+    const species = dex.species.all();
+    log(prefix + 'List_Species', species);
+
+    for (let i in dex.dataCache) {
+        if (i != 'PokemonGoData')
+            log(prefix + 'Data_' + i, dex.dataCache[i]);
+    }
+}
+
+log_gen('gen1');
+log_gen('gen2');
+log_gen('gen3');
+log_gen('gen4');
+log_gen('gen5');
+log_gen('gen6');
+log_gen('gen7');
+log_gen('gen8');
+log_gen('gen9');
