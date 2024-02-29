c38643f3bacd657ab052f75d4dac8721f4576000
lzybluee <lzybluee@gmail.com>
2024-02-29 11:03:49 +0800

    Add zh

--- zh/zh.py
+++ zh/zh.py
@@ -1,6 +1,6 @@
 def en_zh(out, en, zh):
     with open(out, 'w', encoding='utf8') as output:
-        last = ''
+        en_list = []
         with open(en, 'r', encoding='utf8') as input_en:
             en_lines = input_en.readlines()
         with open(zh, 'r', encoding='utf8') as input_zh:
@@ -8,11 +8,11 @@ def en_zh(out, en, zh):
         for i in range(len(en_lines)):
             en_line = en_lines[i].strip()
             zh_line = zh_lines[i].strip()
-            if en_line != zh_line and en_line != last and not en_line.startswith('?') and not en_line.startswith('—'):
+            if en_line != zh_line and en_line not in en_list and not en_line.startswith('?') and not en_line.startswith('—'):
                 output.write(en_line + '\n')
                 output.write(zh_line + '\n')
                 output.write('\n')
-                last = en_line
+                en_list.append(en_line)
 
 
 en_zh('Items_zh.txt',
